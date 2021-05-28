34838ad17085f3870cd95b2b32ac7abea2a03d39
Mihail Mechetin <mihailmecetin@gmail.com>
2021-05-28 16:06:08 +0200

    Add filename input

--- gitDl.lua
+++ gitDl.lua
@@ -1,4 +1,10 @@
-local url = "https://raw.githubusercontent.com/MihailMecetin/ComputerCraft/master/fell.lua"
+local USERNAME = "MihailMecetin"
+local REPOSITORY = "ComputerCraft"
+
+print("Input github filename for user ".. USERNAME .. " and repo ".. REPOSOTORY)
+input filename
+
+local url = "https://raw.githubusercontent.com/".. USERNAME .."/".. REPOSITORY .."/master/".. filename ..".lua"
 
 content = http.get(url).readAll()
 
